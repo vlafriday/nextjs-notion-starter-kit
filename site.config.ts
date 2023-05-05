2ae29c24a0a92acd893e27aa16c409101e2354a0
vlafriday <pakiboy@yandex.ru>
2023-05-05 07:48:02 +0300

    add navigationStyle: 'custom' about and contact

--- site.config.ts
+++ site.config.ts
@@ -42,24 +42,24 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  pageUrlOverrides: {
-    '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-    '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  }
-  // pageUrlOverrides: null,
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null,
 
-  // // whether to use the default notion navigation style or a custom one with links to
-  // // important pages
-  // // navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'af7bd9afcfda4fde805684ad76f4e26d' //my
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: 'a92d3439695b41c0b813724811e2edb8' //my
-  //   }
-  // ]
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: 'af7bd9afcfda4fde805684ad76f4e26d' //my
+    },
+    {
+      title: 'Contact',
+      pageId: 'a92d3439695b41c0b813724811e2edb8' //my
+    }
+  ]
 })
